3386b192e22c73bafbf25ae061ed0bc2994b8a55
Yuyan Li <yuyan.li@gmx.net>
2013-06-10 17:00:54 +0200

    Use a deepcopy to leave orginal list unchanged

--- example_2.py
+++ example_2.py
@@ -1,15 +1,18 @@
+# FIXED
+
 # In the following example, we start off with a list of values, and we add some
 # new values to them. We now want to find the difference between the mean of
 # the original list of values, and the new list of values. This should give
 # 0.625 but it gives 0.0 instead. Why is this?
 
+from copy import deepcopy
 import numpy as np
 
 # Define the original set of values.
 values = [8., 7., 9., 4., 6., 7., 8., 4.]
 
 # Create a new list with the original values and add a few more
-new_values = values
+new_values = deepcopy(values)
 new_values.append(8.)
 new_values.append(9.)
 new_values.append(2.)
